411e208a9823be79db0b4b1d167267a113303eb6
valentin122 <val1915@yandex.ru>
2019-07-07 00:27:20 +0400

    добавил информацию в readme. сменил версию java с 11 на 8.Добавил информационное сообщение при вводе из консоли.

--- src/main/java/ru/zhigalov/input/ConsoleInput.java
+++ src/main/java/ru/zhigalov/input/ConsoleInput.java
@@ -34,7 +34,7 @@ public class ConsoleInput {
                             continue;
                         }
                         temp.add(new TestRow(row[0],row[1],row[2],row[3],row[4],row[5],row[6])); //,row[7]
-                        System.out.println("Строка добавлена");
+                        System.out.println("Строка добавлена. Наберите exit, или добавьте новую строку:");
                     }
                 }catch (IOException ioe){
                     logger.warning("Ошибка при вводе строки с данными");
